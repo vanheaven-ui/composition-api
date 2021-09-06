9b4d940ada7001232e8b150f96c85245351d6598
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-09-05 23:26:35 -0700

    Create PostList component

---
+++ src/components/PostList.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2 class="post-list">Post list</h2>
+  <div v-for="post in posts" :key="post.id">
+    <h3>{{ post.title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["posts"],
+  setup(props) {
+    console.log(props);
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,33 @@
 <template>
   <h1>Home page</h1>
+  <PostList :posts="posts" />
 </template>
 
 <script>
+import { ref } from "@vue/reactivity";
+import PostList from "../components/PostList.vue";
 export default {
+  components: { PostList },
   name: "Home",
+  setup() {
+    const posts = ref([
+      {
+        id: 1,
+        title: "Welcome to the new blog",
+        body: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores at numquam ratione veniam. Optio ab blanditiis pariatur veniam, deserunt odit sapiente quaerat aliquam vel porro voluptate voluptates, officiis quia beatae, dolores at ipsam? Minus id perspiciatis at itaque asperiores deleniti! Quas nam architecto maiores corporis mollitia! Ipsa voluptatum voluptas dignissimos fuga, veritatis atque rerum eligendi quidem accusantium amet quas delectus suscipit itaque quae similique voluptates. Voluptatibus illum, ea vel quaerat quae quos distinctio ducimus quasi reprehenderit quas perspiciatis officiis animi. Cum unde non facere deserunt maiores aspernatur nam illo, error pariatur porro asperiores quae, iusto distinctio rerum, eveniet reiciendis alias.",
+        tags: ["webdev", "coding", "news"],
+      },
+      {
+        id: 2,
+        title: "top 5 CSS tips",
+        body: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores at numquam ratione veniam. Optio ab blanditiis pariatur veniam, deserunt odit sapiente quaerat aliquam vel porro voluptate voluptates, officiis quia beatae, dolores at ipsam? Minus id perspiciatis at itaque asperiores deleniti! Quas nam architecto maiores corporis mollitia! Ipsa voluptatum voluptas dignissimos fuga, veritatis atque rerum eligendi quidem accusantium amet quas delectus suscipit itaque quae similique voluptates. Voluptatibus illum, ea vel quaerat quae quos distinctio ducimus quasi reprehenderit quas perspiciatis officiis animi. Cum unde non facere deserunt maiores aspernatur nam illo, error pariatur porro asperiores quae, iusto distinctio rerum, eveniet reiciendis alias.",
+        tags: ["CSS", "coding", "webdev"],
+      },
+    ]);
+
+    return {
+      posts,
+    };
+  },
 };
 </script>
